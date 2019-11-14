eb5656cc489a3e7f20bbb52c4fb53add544a045a
anastasia1311 <nastiadardinskaya@tut.by>
2019-11-14 22:38:20 +0300

    Adding new change file

---
+++ src/changevalues2.js
@@ -0,0 +1,23 @@
+function change2(value2)
+{
+switch(value)
+{
+
+    case "Ivanov":
+        value2="Iv";
+        break;
+    case "Petrov":
+        value2="Pet";
+        break;
+    case "Sidorov":
+        value2="Sid";
+        break;
+    case "Suvorov":
+        value2="Suv";
+        break;
+    default:
+        alert("nope");
+}
+return value2;
+
+}
